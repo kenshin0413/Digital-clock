ce91def4fcddf76e1fb1b6b81d73e225c1cf836c
kenshin0413 <kenshin1234569@gmail.com>
2025-09-28 22:54:30 +0900

    初期

---
+++ Digital-clock/Digital-clock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Digital-clock
+//
+//  Created by miyamotokenshin on R 7/09/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Digital-clock/Digital-clock/Digital_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_clockApp.swift
+//  Digital-clock
+//
+//  Created by miyamotokenshin on R 7/09/28.
+//
+
+import SwiftUI
+
+@main
+struct Digital_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
